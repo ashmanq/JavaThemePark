8b3ba555f0d6b55cdcebefbd6248ab45042884ac
Ashir <ashirq@outlook.com>
2020-06-02 18:20:30 +0100

    Added method that creates a list of all attraction visitors are allowed to go on.

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,46 +1,82 @@
 import attractions.Attraction;
 import behaviours.IReviewed;
+import behaviours.ISecurity;
+import com.sun.codemodel.internal.JForEach;
 import people.Visitor;
 import stalls.Stall;
 
 import javax.smartcardio.ATR;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ThemePark {
 
-    private ArrayList<Attraction> attractions;
-    private ArrayList<Stall> stalls;
     private ArrayList<IReviewed> reviewedPlaces;
     String name;
 
     public ThemePark(String name) {
+
         this.name = name;
-        this.attractions = new ArrayList<>();
-        this.stalls = new ArrayList<>();
         this.reviewedPlaces = new ArrayList<>();
+
     }
 
     public String getName() {
+
         return name;
+
     }
 
 
     public int getNoReviewedPlaces() {
+
         return this.reviewedPlaces.size();
+
     }
 
 
     public void addReviewedPlace(IReviewed place) {
+
         this.reviewedPlaces.add(place);
+
     }
 
     public ArrayList<IReviewed> getAllReviewed() {
+
         return this.reviewedPlaces;
+
     }
 
     public void visit(Visitor visitor, Attraction attraction) {
+
         visitor.addVisitedAttraction(attraction);
         attraction.incrementVisitCount(1);
+
+    }
+
+    public HashMap<String, Integer> getReviews() {
+        HashMap<String, Integer> reviews = new HashMap<>();
+
+        for(IReviewed reviewedPlace : this.reviewedPlaces) {
+            String name = reviewedPlace.getName();
+            Integer score = reviewedPlace.getRating();
+            reviews.put(name, score);
+        }
+        return reviews;
+    }
+
+    public ArrayList<IReviewed> getAllAllowedFor(Visitor visitor) {
+        ArrayList<IReviewed> allowedList = new ArrayList<>();
+
+        for(IReviewed reviewedPlace : this.reviewedPlaces) {
+
+            if(!(reviewedPlace instanceof ISecurity)) {
+                allowedList.add(reviewedPlace);
+            } else if(reviewedPlace instanceof ISecurity && ((ISecurity) reviewedPlace).isAllowedTo(visitor)) {
+                allowedList.add(reviewedPlace);
+            }
+        }
+        return allowedList;
     }
 
 }
--- src/test/java/ThemeParkTest.java
+++ src/test/java/ThemeParkTest.java
@@ -1,15 +1,13 @@
-import attractions.Attraction;
-import attractions.DodgemTest;
-import attractions.Dodgems;
-import attractions.RollerCoaster;
+import attractions.*;
+import behaviours.IReviewed;
 import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Attr;
 import people.Visitor;
-import stalls.CandyflossStall;
-import stalls.ParkingSpot;
-import stalls.Stall;
+import stalls.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.CancellationException;
 
 import static org.junit.Assert.assertEquals;
@@ -68,5 +66,52 @@ public class ThemeParkTest {
         themepark.visit(visitor, rollerCoaster);
         assertEquals(1, rollerCoaster.getVisitCount());
         assertEquals(1, visitor.getVisitedAttractions().size());
+    }
+
+    @Test
+    public void canGetAllReviewScoresWithNames() {
+
+        Dodgems dodgems = new Dodgems("A Dodgem", 1);
+        RollerCoaster rollerCoaster = new RollerCoaster("A Rollercoaster", 2);
+        TobaccoStall tobaccoStall = new TobaccoStall("A Tobacco Stall", "Jim J", ParkingSpot.A2,7 );
+        IceCreamStall iceCreamStall = new IceCreamStall("An Icecream stall", "Brian B", ParkingSpot.A3, 9);
+
+        themepark.addReviewedPlace(dodgems);
+        themepark.addReviewedPlace(rollerCoaster);
+        themepark.addReviewedPlace(tobaccoStall);
+        themepark.addReviewedPlace(iceCreamStall);
+
+        HashMap<String,Integer> list = themepark.getReviews();
+
+        list.get("A Rollercoaster");
+
+        assertEquals(1, (int)list.get("A Dodgem"));
+        assertEquals(2, (int)list.get("A Rollercoaster"));
+        assertEquals(7, (int)list.get("A Tobacco Stall"));
+        assertEquals(9, (int)list.get("An Icecream stall"));
+    }
+
+    @Test
+    public void canGetAllowedForList() {
+        Park park = new Park("Park", 8);
+        Playground playground = new Playground("Playground", 7);
+        Dodgems dodgems = new Dodgems("A Dodgem", 1);
+        RollerCoaster rollerCoaster = new RollerCoaster("A Rollercoaster", 2);
+        TobaccoStall tobaccoStall = new TobaccoStall("A Tobacco Stall", "Jim J", ParkingSpot.A2,7 );
+        IceCreamStall iceCreamStall = new IceCreamStall("An Icecream stall", "Brian B", ParkingSpot.A3, 9);
+        CandyflossStall candyflossStall = new CandyflossStall("Candyfloss", "Leam L", ParkingSpot.B1,4);
+
+        themepark.addReviewedPlace(playground);
+        themepark.addReviewedPlace(park);
+        themepark.addReviewedPlace(dodgems);
+        themepark.addReviewedPlace(rollerCoaster);
+        themepark.addReviewedPlace(tobaccoStall);
+        themepark.addReviewedPlace(iceCreamStall);
+        themepark.addReviewedPlace(candyflossStall);
+
+        Visitor visitor = new Visitor(10, 100, 100.00);
+        ArrayList<IReviewed> allowedList = themepark.getAllAllowedFor(visitor);
+
+        assertEquals(5, allowedList.size());
     }
 }
